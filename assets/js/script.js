71a8f30559ecb6e2cf0125dd39f78e5834921507
TREVTAL <trevtal@gmail.com>
2022-07-15 21:52:44 -0500

    color code vs moment
    coded if statements to color code time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // setup current date
-var now = moment();
+var today = moment();
 // set text so current date is printed on top os page
-$('#currentDay').text(now.format("dddd, MMMM Do"));
+$('#currentDay').text(today.format("dddd, MMMM Do"));
 
 // event loader
 var h0600 = localStorage.getItem('ls0600');
@@ -13,5 +13,15 @@ btn06.on('click', function(){
     h0600 = $('#html0600').html();
     console.log(h0600);
     localStorage.setItem('ls0600', h0600);
-})
+});
 
+// define class based on hour
+
+var hr=moment().format("HH");
+console.log(hr);
+
+if (hr==06) {
+    $('#html0600').addClass('present')
+} else if (hr>06) {
+    $('#html0600').addClass('past')
+}  else {   $('#html0600').addClass('future') };
