a08af75221bcceb3ff1eec7ac45d32ba87c1df0a
TREVTAL <trevtal@gmail.com>
2022-07-15 21:05:12 -0500

    setup local storage
    setup both setItem on save and getItem on pageload for 0600

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 // setup current date
 var now = moment();
-
+// set text so current date is printed on top os page
 $('#currentDay').text(now.format("dddd, MMMM Do"));
 
-// var h0600 = $('.html0600').html();
-// console.log(h0600);
+// event loader
+var h0600 = localStorage.getItem('ls0600');
+$('#html0600').html(h0600);
 
+// event saver
 var btn06 = $('#save06');
 btn06.on('click', function(){
-    var h0600 = $('#html0600').html();
+    h0600 = $('#html0600').html();
     console.log(h0600);
+    localStorage.setItem('ls0600', h0600);
 })
 
